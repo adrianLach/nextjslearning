5f201526527c440fff934779b6a393a8242fdd3f
Adrian Lach <adrian.lach1999@gmail.com>
2023-07-13 19:18:56 +0200

    Cypress Workflow Action Test (Should fail)

--- cypress/e2e/sitereachable.cy.ts
+++ cypress/e2e/sitereachable.cy.ts
@@ -2,4 +2,10 @@ describe('Navigation', () => {
     it('should navigate the index page', () => {
         cy.visit('http://localhost:3000')
     })
+    it('should navigate the about page', () => {
+        cy.visit('http://localhost:3000/about')
+    })
+    it('should navigate the contact page', () => {
+        cy.visit('http://localhost:3000/contact')
+    })
 })
