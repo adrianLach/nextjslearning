0e011b8ddb6d743392a50e1b5f76937188b10da0
Adrian Lach <adrian.lach1999@gmail.com>
2023-07-14 17:30:16 +0200

    Adding Cat Page

---
+++ pages/cat.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import { useState } from "react"
+
+const Index = () => {
+
+    const [image, setImage] = useState<{url: string, width: number, height: number}>()
+
+    const getCat = async () => {
+        const cat = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=${process.env.CAT_KEY || ''}`, {})
+        .then(r => r.json())
+
+        setImage(cat[0])
+
+        console.log(cat[0].url)
+
+    }
+
+    return (
+        <>
+            <div style={{display: 'flex', alignContent: 'center', justifyContent: 'center', margin: '20px'}}>
+                <button style={{width: '20%', padding: '10px'}} onClick={() => getCat()}>Get Cat</button>
+            </div>
+            {image && <img src={image.url} width={'100%'} alt="cat"></img>}
+        </>
+    )
+}
+
+export default Index
