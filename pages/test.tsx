5bee908f4740d94f85b8b12f8d90a2b729159ccd
adrianLach <54017380+adrianLach@users.noreply.github.com>
2023-10-07 17:37:39 +0200

    Update test.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,5 @@
 import { Button, Flex, Spacer, Stack, Td, Th, Tr } from '@chakra-ui/react'
 import { PlusSquareIcon } from '@chakra-ui/icons'
-import TableCard from 'components/TableCard'
 
 const Test = () => {
 
@@ -19,48 +18,6 @@ const Test = () => {
                         </Button>
                     </Flex>
                 </Stack>
-                <TableCard
-                    title='Income'
-                    th={<><Th>Description</Th><Th isNumeric>Amount</Th></>}
-                    trs={
-                        <>
-                            <Tr>
-                                <Td>Salary</Td>
-                                <Td isNumeric>{currency(2050)}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>Papa</Td>
-                                <Td isNumeric>{currency(400)}</Td>
-                            </Tr>
-                        </>
-                    }
-                    footerText={'Total: ' + currency(2450)}
-                />
-                <TableCard
-                    title='Apartment'
-                    th={<><Th>Description</Th><Th isNumeric>Amount</Th></>}
-                    trs={
-                        <>
-                            <Tr>
-                                <Td>Rent</Td>
-                                <Td isNumeric>{currency(958.44)}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>Electricity</Td>
-                                <Td isNumeric>{currency(53)}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>Internet Provider</Td>
-                                <Td isNumeric>{currency(59.70)}</Td>
-                            </Tr>
-                            <Tr>
-                                <Td>ARD</Td>
-                                <Td isNumeric>{currency(18.36)}</Td>
-                            </Tr>
-                        </>
-                    }
-                    footerText={'Total: ' + currency(1089.50)}
-                />
             </Stack>
         </>
     )
